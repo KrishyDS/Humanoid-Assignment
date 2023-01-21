657b35d3aa5374c95e960cf4812fc0a33bffdb50
Krish Shethiya <75296946+KrishyDS@users.noreply.github.com>
2023-01-21 15:47:10 +0530

    Update modelclass.py

--- modelclass.py
+++ modelclass.py
@@ -2,16 +2,17 @@ import speech_recognition as sr
 import pyttsx3
 import os
 
+#creating class object
 class VoiceModel:
     def __init__(self):
         self.r = sr.Recognizer()
 
     def listen(self):
         with sr.Microphone() as source:
-            self.r.adjust_for_ambient_noise(source, duration=0.2)
+            self.r.adjust_for_ambient_noise(source, duration=0.2) ## adjusting for noise
             audio = self.r.listen(source)
             try:
-                text = self.r.recognize_google(audio)
+                text = self.r.recognize_google(audio) ## using google's speech recognition engine for Text to speech
                 return text
             except:
                 return "Sorry, I didn't understand what you said."
